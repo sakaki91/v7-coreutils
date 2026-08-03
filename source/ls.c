13401fb730a03fc19b6ff67a353fe204cbae5dc7
sakaki91 <sakaki@tuta.io>
2026-08-03 14:16:12 -0300

    Update (ls - v1.4)

--- source/ls.c
+++ source/ls.c
@@ -3,28 +3,23 @@
 #include <string.h>
 #include <dirent.h>
 
-// l - v1.3
+// ls - v1.4
 // Copyright (C) by Sakaki, 2026.
 // LICENSE: BSD 3-Clause License <https://opensource.org/license/bsd-3-clause>
 
-void listDirectory(DIR *dir){
-    struct dirent *entry;
-    if (dir == NULL){
-        perror("l");
+int main(int argc, char *argv[]){
+    DIR *dir; struct dirent *entry;
+    dir = (argc < 2) ? opendir(".") : opendir(argv[1]);
+    if(dir == NULL){
+        perror("ls");
         exit(EXIT_FAILURE);
     }
-    while((entry = readdir(dir)) != NULL){
+    while((entry = readdir (dir)) != NULL){
         if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
             continue;
         }
         printf("%s\n", entry->d_name);
     }
-}
-
-int main(int argc, char *argv[]){
-    DIR *dir;
-    dir = (argc > 1) ? opendir(argv[1]) : opendir(".");
-    listDirectory(dir);
     closedir(dir);
     return 0;
 }
