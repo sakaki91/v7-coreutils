d03f7db2cb93f05ed433d68296b11a49003d011a
sakaki91 <sakaki@tuta.io>
2026-08-03 02:04:54 -0300

    Update - (l - v1.3 (fclose correction))

--- source/l.c
+++ source/l.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <dirent.h>
 
-// l - v1.2
+// l - v1.3
 // Copyright (C) by Sakaki, 2026.
 // LICENSE: BSD 3-Clause License <https://opensource.org/license/bsd-3-clause>
 
@@ -17,14 +17,14 @@ void listDirectory(DIR *dir){
         if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
             continue;
         }
-        printf("%s\n", entry->d_name);
+        printf(" %s\n", entry->d_name);
     }
-    closedir(dir);
 }
 
 int main(int argc, char *argv[]){
     DIR *dir;
     dir = (argc > 1) ? opendir(argv[1]) : opendir(".");
     listDirectory(dir);
+    closedir(dir);
     return 0;
 }
