9798028b09978af7409a4d44cb979049140f758c
sakaki91 <sakaki@tuta.io>
2026-08-03 02:05:50 -0300

    Update - (l - v1.3 (fclose correction))

--- source/l.c
+++ source/l.c
@@ -17,7 +17,7 @@ void listDirectory(DIR *dir){
         if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
             continue;
         }
-        printf(" %s\n", entry->d_name);
+        printf("%s\n", entry->d_name);
     }
 }
 
