d166e418fdb36541a03fb0ec373928f29505bca6
sakaki91 <sakaki@tuta.io>
2026-08-05 01:48:18 -0300

    Update (uname v2.0)
    The uname utility was literally refactored, as it was incomplete and unstable, and is now so functional that the Xorg graphical server can boot using it instead of GNU uname.

--- src/uname.c
+++ src/uname.c
@@ -1,30 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <sys/utsname.h>
 
-// uname - v1.0
+// uname - v2.0
 // Copyright (C) by Sakaki, 2026.
 // LICENSE: BSD 3-Clause License <https://opensource.org/license/bsd-3-clause>
 
 int main(int argc, char *argv[]){
     struct utsname buffer;
+    int args;
+    bool s = false;
+    bool r = false;
+    bool v = false;
+    bool n = false;
+    bool m = false;
     if(uname(&buffer) != 0){
         perror("uname");
     }
     if (argc < 2){
-        printf("%s %s\n", buffer.sysname, buffer.release);
+        printf("%s\n", buffer.sysname);
     } else {
-        if (strcmp(argv[1], "-v") == 0){
-            printf("%s %s %s\n", buffer.sysname, buffer.release, buffer.version);
-        } else if (strcmp(argv[1], "-h") == 0){
-            fprintf(stdout, "\033[4muname\033[0m\n"
-                    "-h    show this message.\n"
-                    "-v    show kernel version.\n"
-                   );
-        } else {
-            fprintf(stderr, "error, type -h to see the options.\n");
-            exit(EXIT_FAILURE);
+        if (strcmp(argv[1], "-h") == 0){
+            fprintf(stdout, "\033[4moperations:\033[0m\n"
+                            "-h   show this message.\n"
+                            "-s   print the kernel name.\n"
+                            "-r   print the kernel release.\n"
+                            "-v   print the kernel version.\n"
+                            "-n   print the hostname.\n"
+                            "-m   print the machine hardware name.\n\n"
+                            "usage: uname <operation>\n");
+            exit(EXIT_SUCCESS);
         }
-    }   
+        while ((args = getopt(argc, argv, "srvnm")) != -1){
+            switch (args){
+                case 's':
+                    s = true;
+                    break;
+                case 'r':
+                    r = true;
+                    break;
+                case 'v':
+                    v = true;
+                    break;
+                case 'n':
+                    n = true;
+                    break;
+                case 'm':
+                    m = true;
+                    break;
+            }
+        }
+        if (s == true){
+            printf("%s", buffer.sysname);
+        }
+        if (r == true){
+            printf("%s", buffer.release);
+        }
+        if (v == true){
+            printf("%s", buffer.version);
+        }
+        if (n == true){
+            printf("%s", buffer.nodename);
+        } 
+        if (m == true){
+            printf("%s", buffer.machine);
+        } printf ("\n");
+    }
 }
