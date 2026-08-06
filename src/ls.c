55f1346278f6e21023d985f5731732f7e7b4c74f
sakaki91 <sakaki@tuta.io>
2026-08-06 00:45:04 -0300

    Update (ls.c)

--- src/ls.c
+++ src/ls.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <dirent.h>
 
-// ls - v1.4
+// ls - v1.5
 // Copyright (C) by Sakaki, 2026.
 // LICENSE: BSD 3-Clause License <https://opensource.org/license/bsd-3-clause>
 
@@ -15,10 +14,10 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
     while((entry = readdir (dir)) != NULL){
-        if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0){
+        if (entry->d_name[0] == '.'){
             continue;
-        }
-        printf("%s\n", entry->d_name);
+         }
+         printf("%s\n", entry->d_name);
     }
     closedir(dir);
     return 0;
