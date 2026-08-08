3aff275efce1753a8414e61d4556e8a1d05c9a7f
sakaki91 <sakaki@tuta.io>
2026-08-07 21:11:24 -0300

    Update
    clear 1.1
    =========
    An ANSI escape character has been added to clear not only the screen, but also the history.

--- src/clear.c
+++ src/clear.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// clear - v1.0
+// clear - v1.1
 // Copyright (C) by Sakaki, 2026.
 // LICENSE: BSD 3-Clause License <https://opensource.org/license/bsd-3-clause>
 
 int main(){
-    printf("\033[2J\033[H");
+    printf("\033[2J\033[H\033[3J");
     return 0;
 }
